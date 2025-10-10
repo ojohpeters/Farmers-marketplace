d43ab255a71ac38d8d923cf398a17b911cc049d2
ojochegbe <peterspraisezy>
2025-10-10 07:57:41 +0000

    fixed ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import User from '@/models/User'
 import bcrypt from 'bcryptjs'
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     CredentialsProvider({
       name: 'credentials',
